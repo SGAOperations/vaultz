1263df7c56c78eeef2f3637dfbe56e682f1ff6fe
Benedikt <144247685+b-at-neu@users.noreply.github.com>
2025-08-17 16:44:33 -0400

    #3 create component for create index dialog

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,12 @@
+import { CreateIndexDialog } from '@/components/create-index-dialog';
 import { IndexCard } from '@/components/index-card';
-import { Button } from '@/components/ui/button';
 import { getAllIndexes } from '@/prisma/services';
-import { Plus } from 'lucide-react';
 
 export default async function Home() {
   const indexes = await getAllIndexes();
 
   return (
-    <div className="grid grid-cols-1 lg:grid-cols-2 gap-3">
+    <div className="w-full grid grid-cols-1 lg:grid-cols-2 gap-3">
       {indexes.map((v, i) => (
         <IndexCard
           key={i}
@@ -17,10 +16,8 @@ export default async function Home() {
           purchases={v.purchases}
         />
       ))}
-      <Button>
-        <Plus />
-        Create Index
-      </Button>
+
+      <CreateIndexDialog />
     </div>
   );
 }
---
+++ components/create-index-dialog.tsx
@@ -0,0 +1,35 @@
+'use client';
+
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog';
+import { Plus } from 'lucide-react';
+import { Button } from '@/components/ui/button';
+
+export function CreateIndexDialog() {
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button>
+          <Plus />
+          Create Index
+        </Button>
+      </DialogTrigger>
+
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Are you absolutely sure?</DialogTitle>
+          <DialogDescription>
+            This action cannot be undone. This will permanently delete your
+            account and remove your data from our servers.
+          </DialogDescription>
+        </DialogHeader>
+      </DialogContent>
+    </Dialog>
+  );
+}
