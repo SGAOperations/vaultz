f6bf8c4fb9caa74846cfaf68e2fb5ec9ad2ee93f
Benedikt <144247685+b-at-neu@users.noreply.github.com>
2025-08-17 17:04:00 -0400

    #10 add create index form

--- components/create-index-dialog.tsx
+++ components/create-index-dialog.tsx
@@ -10,8 +10,35 @@ import {
 } from '@/components/ui/dialog';
 import { Plus } from 'lucide-react';
 import { Button } from '@/components/ui/button';
+import { z } from 'zod/v4';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+
+const schema = z.object({
+  name: z.string().min(2).max(50),
+  code: z.string().length(6),
+});
 
 export function CreateIndexDialog() {
+  const form = useForm<z.infer<typeof schema>>({
+    resolver: zodResolver(schema),
+    defaultValues: { name: '', code: '' },
+  });
+
+  function onSubmit(data: z.infer<typeof schema>) {
+    console.log(data);
+  }
+
   return (
     <Dialog>
       <DialogTrigger asChild>
@@ -23,12 +50,46 @@ export function CreateIndexDialog() {
 
       <DialogContent>
         <DialogHeader>
-          <DialogTitle>Are you absolutely sure?</DialogTitle>
+          <DialogTitle>Create Index</DialogTitle>
           <DialogDescription>
-            This action cannot be undone. This will permanently delete your
-            account and remove your data from our servers.
+            An index contains multiple accounts that track purchases.
           </DialogDescription>
         </DialogHeader>
+
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Name</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Budget Index" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="code"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Code</FormLabel>
+                  <FormControl>
+                    <Input placeholder="80XXXX" {...field} />
+                  </FormControl>
+                  <FormDescription>
+                    The index number to be associated with this index.
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type="submit">Submit</Button>
+          </form>
+        </Form>
       </DialogContent>
     </Dialog>
   );
