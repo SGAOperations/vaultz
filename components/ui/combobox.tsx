0bb31893e72279bd66f4d46a7f78201edc392645
Benedikt <144247685+b-at-neu@users.noreply.github.com>
2025-08-22 21:58:14 -0400

    #4 upgrade combobox

--- components/ui/combobox.tsx
+++ components/ui/combobox.tsx
@@ -33,7 +33,7 @@ export function Combobox({
   const [open, setOpen] = React.useState(false);
 
   return (
-    <Popover open={open} onOpenChange={setOpen}>
+    <Popover modal open={open} onOpenChange={setOpen}>
       <PopoverTrigger asChild>
         <Button
           variant="outline"
