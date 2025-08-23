70e91ed0217ae08e18d3a187ca5d4663de783be2
Benedikt <144247685+b-at-neu@users.noreply.github.com>
2025-08-22 22:03:01 -0400

    #4 create large number display util

--- app/index/[indexId]/page.tsx
+++ app/index/[indexId]/page.tsx
@@ -1,5 +1,6 @@
 import { CreatePurchaseDialog } from '@/components/create-purchase-dialog';
 import { Card } from '@/components/ui/card';
+import { formatNumber } from '@/lib/utils';
 import { getIndex } from '@/prisma/services';
 import { getAccountsByIndex } from '@/prisma/services/account';
 import { getUsers } from '@/prisma/services/user';
@@ -27,15 +28,15 @@ export default async function Index({
     <div className="w-full">
       <div className="w-full grid grid-cols-1 md:grid-cols-3 gap-4">
         <Card className="flex-row items-baseline">
-          <p className="text-6xl">${index.amount}</p>
+          <p className="text-6xl">${formatNumber(index.amount)}</p>
           <p className="text-muted-foreground text-sm">Total</p>
         </Card>
         <Card className="flex-row items-baseline">
-          <p className="text-6xl">${spent}</p>
+          <p className="text-6xl">${formatNumber(spent)}</p>
           <p className="text-muted-foreground text-sm">Spent</p>
         </Card>
         <Card className="flex-row items-baseline">
-          <p className="text-6xl">${index.amount - spent}</p>
+          <p className="text-6xl">${formatNumber(index.amount - spent)}</p>
           <p className="text-muted-foreground text-sm">Remaining</p>
         </Card>
         <CreatePurchaseDialog users={users} accounts={accounts} />
