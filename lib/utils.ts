ff6bc2da5560d4a3107017b9d8b8e708fd44be7e
Benedikt <144247685+b-at-neu@users.noreply.github.com>
2025-08-22 23:01:08 -0400

    #4 update format number to cut more than 2 decimals

--- lib/utils.ts
+++ lib/utils.ts
@@ -6,5 +6,6 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 export function formatNumber(value: number) {
+  value = Math.round(value * 100) / 100;
   return value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
 }
