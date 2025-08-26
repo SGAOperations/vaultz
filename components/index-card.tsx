bfdbbbce3855cdc4239332a69ac1a8c2379a8566
Benedikt <144247685+b-at-neu@users.noreply.github.com>
2025-08-25 21:22:12 -0400

    #33 add hover colors for cards

--- components/index-card.tsx
+++ components/index-card.tsx
@@ -14,7 +14,7 @@ export function IndexCard({
 }) {
   return (
     <Link href={`/index/${id}`}>
-      <Card className="h-full">
+      <Card className="h-full group hover:bg-accent">
         <CardHeader>
           <CardTitle>
             {name} ({code})
@@ -30,7 +30,7 @@ export function IndexCard({
 
         <div className="px-6 flex flex-col gap-3">
           {purchases.slice(0, 3).map((v) => (
-            <Card key={v.id} className="flex-row justify-between p-3">
+            <Card key={v.id} className="flex-row justify-between p-3 group-hover:bg-accent group-hover:border-muted">
               <p>${v.amount}</p>
               <p>
                 {v.user.first} {v.user.last}
