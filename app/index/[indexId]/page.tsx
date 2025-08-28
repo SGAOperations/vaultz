b0d439a0b9e05327ba92a10735619e48cb61a6c7
Benedikt <144247685+b-at-neu@users.noreply.github.com>
2025-08-27 21:30:26 -0400

    #25 increase spacing for accounts

--- app/index/[indexId]/page.tsx
+++ app/index/[indexId]/page.tsx
@@ -55,7 +55,7 @@ export default async function Index({
       </div>
 
       <h2 className="mt-4 text-xl">Accounts</h2>
-      <div className="grid grid-cols-4 gap-3">
+      <div className="grid grid-cols-3 gap-3">
         {accounts.map((account) => (
           <Card key={account.id} className="flex flex-row justify-between py-3">
             <p>
