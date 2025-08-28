ad5574f3a88fb6af0f959192245435104ad6b226
Benedikt <144247685+b-at-neu@users.noreply.github.com>
2025-08-27 21:30:02 -0400

    #25 display account name in cards

--- app/index/[indexId]/page.tsx
+++ app/index/[indexId]/page.tsx
@@ -58,7 +58,9 @@ export default async function Index({
       <div className="grid grid-cols-4 gap-3">
         {accounts.map((account) => (
           <Card key={account.id} className="flex flex-row justify-between py-3">
-            <p>{account.code}</p>
+            <p>
+              {account.name} ({account.code})
+            </p>
             <p>
               $
               {formatNumber(
