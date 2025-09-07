444eb24faca269be8021845f28ed3665647fb510
Benedikt <144247685+b-at-neu@users.noreply.github.com>
2025-09-07 15:21:12 -0400

    #28 add specific file upload types

--- app/api/uploadthing/core.ts
+++ app/api/uploadthing/core.ts
@@ -3,7 +3,10 @@ import { type FileRouter, createUploadthing } from 'uploadthing/next';
 const f = createUploadthing();
 
 export const receiptFileRouter = {
-  receipts: f({ pdf: { maxFileCount: 1, maxFileSize: '512KB' } }).onUploadComplete(() => ({}))
+  receipts: f({
+    image: { maxFileCount: 5, maxFileSize: '1MB' },
+    pdf: { maxFileCount: 5, maxFileSize: '512KB' },
+  }).onUploadComplete(() => ({})),
 } satisfies FileRouter;
 
 export type ReceiptFileRouter = typeof receiptFileRouter;
