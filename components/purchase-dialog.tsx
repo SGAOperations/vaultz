8b1c890122be9930541e63e7799e9fd805f8daf5
Benedikt <144247685+b-at-neu@users.noreply.github.com>
2025-09-19 08:59:20 -0400

    #29 create purchase info dialog

--- components/purchase-dialog.tsx
+++ components/purchase-dialog.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { PurchaseWithUser } from '@/lib/types';
+import { formatNumber, getFileUrl } from '@/lib/utils';
 
 import {
   Dialog,
@@ -20,24 +21,43 @@ export function PurchaseDialog({
   trigger: React.ReactNode;
   purchase: PurchaseWithUser;
 }) {
+  const receiptUrls = purchase.receipts.map((r) => getFileUrl(r));
+
   return (
     <Dialog>
       <DialogTrigger>{trigger}</DialogTrigger>
-      <DialogContent>
+      <DialogContent className="sm:max-w-1/3">
         <DialogHeader>
           <DialogTitle>Purchase Information</DialogTitle>
           <DialogDescription>
             See all of the details relevant to this purchase.
           </DialogDescription>
-
-          <div>
-            <p>{purchase.id}</p>
-            <p>
-              {purchase.user.first} {purchase.user.last}
-            </p>
-            <DateTime date={purchase.timestamp} />
-          </div>
         </DialogHeader>
+
+        <div className="grid grid-cols-2 gap-4">
+          <p>Purchase ID</p>
+          <p className="text-muted-foreground">{purchase.id}</p>
+          <p>Date</p>
+          <p className="text-muted-foreground">
+            <DateTime date={purchase.timestamp} />
+          </p>
+          <p>Name</p>
+          <p className="text-muted-foreground">
+            {purchase.user.first} {purchase.user.last}
+          </p>
+          <p>Amount</p>
+          <p className="text-muted-foreground">
+            ${formatNumber(purchase.amount)}
+          </p>
+          <p>Receipts</p>
+          <p className="text-muted-foreground flex gap-3">
+            {receiptUrls.map((url, i) => (
+              <a key={url} href={url} target="_blank" className="hover:underline">
+                File {i}
+              </a>
+            ))}
+          </p>
+        </div>
       </DialogContent>
     </Dialog>
   );
