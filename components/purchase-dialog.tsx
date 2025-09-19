4196c9d9399a1e5865e2df7737825c8d26aa92ac
Benedikt <144247685+b-at-neu@users.noreply.github.com>
2025-09-19 09:00:06 -0400

    #29 add empty message for receipts

--- components/purchase-dialog.tsx
+++ components/purchase-dialog.tsx
@@ -56,6 +56,7 @@ export function PurchaseDialog({
                 File {i}
               </a>
             ))}
+            {receiptUrls.length === 0 && 'N/A'}
           </p>
         </div>
       </DialogContent>
