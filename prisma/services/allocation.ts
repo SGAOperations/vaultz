45450a872832b2f1c736049735d90cb9cacd049c
Benedikt <144247685+b-at-neu@users.noreply.github.com>
2025-09-20 15:31:02 -0400

    #73 create service for getting allocations by id

--- prisma/services/allocation.ts
+++ prisma/services/allocation.ts
@@ -3,6 +3,7 @@
 import { revalidatePath } from 'next/cache';
 
 import prisma from '@/lib/prisma';
+import { AllocationWithPurchases } from '@/lib/types';
 
 export async function createAllocation({
   name,
@@ -20,4 +21,26 @@ export async function createAllocation({
   revalidatePath('/allocation-groups');
 
   return { ...allocation, amount: allocation.amount.toNumber() };
+}
+
+export async function getAllocationById({
+  id,
+}: {
+  id: string;
+}): Promise<AllocationWithPurchases | null> {
+  const allocation = await prisma.allocation.findUnique({
+    where: { id },
+    include: { purchases: { include: { user: true } } },
+  });
+
+  if (allocation === null) return null;
+
+  return {
+    ...allocation,
+    amount: allocation.amount.toNumber(),
+    purchases: allocation.purchases.map((purchase) => ({
+      ...purchase,
+      amount: purchase.amount.toNumber(),
+    })),
+  };
 }
