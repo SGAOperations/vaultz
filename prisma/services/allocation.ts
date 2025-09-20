545679deb1839f876bebf8082a37c654788111a1
Benedikt <144247685+b-at-neu@users.noreply.github.com>
2025-09-20 10:45:07 -0400

    #73 add service to create allocation

---
+++ prisma/services/allocation.ts
@@ -0,0 +1,19 @@
+'use server';
+
+import { revalidatePath } from 'next/cache';
+
+import prisma from '@/lib/prisma';
+
+export async function createAllocation({
+  name,
+  amount,
+}: {
+  name: string;
+  amount: number;
+}) {
+  const allocation = await prisma.allocation.create({ data: { name, amount } });
+
+  revalidatePath('/allocation-groups');
+
+  return allocation;
+}
