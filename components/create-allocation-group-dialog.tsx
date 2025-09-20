9ce8034a985e65d9f78b0a6e5e985d7e81646ec6
Benedikt <144247685+b-at-neu@users.noreply.github.com>
2025-09-20 10:17:47 -0400

    #73 add dialog for creating allocation groups

---
+++ components/create-allocation-group-dialog.tsx
@@ -0,0 +1,88 @@
+'use client';
+
+import { useState } from 'react';
+import { useForm } from 'react-hook-form';
+
+import { zodResolver } from '@hookform/resolvers/zod';
+import { z } from 'zod/v4';
+
+import { createAllocationGroup } from '@/prisma/services/allocation-groups';
+
+import { Button } from '@/components/ui/button';
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+
+const schema = z.object({
+  name: z
+    .string()
+    .min(2, 'Must be at least 2 characters')
+    .max(50, 'Cannot be longer than 50 characters'),
+});
+
+export function CreateAllocationGroupDialog({
+  trigger,
+}: {
+  trigger: React.ReactNode;
+}) {
+  const [open, setOpen] = useState<boolean>(false);
+  const form = useForm<z.infer<typeof schema>>({
+    resolver: zodResolver(schema),
+    defaultValues: { name: '' },
+  });
+
+  function onSubmit(data: z.infer<typeof schema>) {
+    createAllocationGroup(data);
+    form.reset();
+    setOpen(false);
+  }
+
+  return (
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogTrigger asChild>{trigger}</DialogTrigger>
+
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Create Allocation Group</DialogTitle>
+          <DialogDescription>
+            An allocation group contains multiple allocations that track
+            expenses.
+          </DialogDescription>
+        </DialogHeader>
+
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Name</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Office of the President" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type="submit">Submit</Button>
+          </form>
+        </Form>
+      </DialogContent>
+    </Dialog>
+  );
+}
