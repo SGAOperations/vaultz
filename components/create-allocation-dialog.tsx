f4ed71aee2185e36afa4e31ba7c3174c95e850fe
Benedikt <144247685+b-at-neu@users.noreply.github.com>
2025-09-20 10:49:16 -0400

    #73 add dialog to create allocations

---
+++ components/create-allocation-dialog.tsx
@@ -0,0 +1,111 @@
+'use client';
+
+import { useState } from 'react';
+import { useForm } from 'react-hook-form';
+
+import { zodResolver } from '@hookform/resolvers/zod';
+import { z } from 'zod/v4';
+
+import { createAllocation } from '@/prisma/services/allocation';
+
+import { Button } from '@/components/ui/button';
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+
+const schema = z.object({
+  name: z
+    .string()
+    .min(2, 'Must be at least 2 characters')
+    .max(50, 'Cannot be longer than 50 characters'),
+  amount: z.coerce
+    .number<number>()
+    .min(0.01, 'Must be at least $0.01')
+    .multipleOf(0.01, 'Must contain at most 2 decimal places'),
+});
+
+export function CreateAllocationDialog({
+  trigger,
+}: {
+  trigger: React.ReactNode;
+}) {
+  const [open, setOpen] = useState<boolean>(false);
+  const form = useForm<z.infer<typeof schema>>({
+    resolver: zodResolver(schema),
+    defaultValues: { name: '', amount: 0 },
+  });
+
+  function onSubmit(data: z.infer<typeof schema>) {
+    createAllocation(data);
+    form.reset();
+    setOpen(false);
+  }
+
+  return (
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogTrigger asChild>{trigger}</DialogTrigger>
+
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Create Allocation</DialogTitle>
+          <DialogDescription>
+            An allocation is a budgeted amount of money for a specific purpose.
+          </DialogDescription>
+        </DialogHeader>
+
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Name</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Office of the President" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="amount"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Amount</FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder="$21.45"
+                      {...field}
+                      value={field.value ? '$' + field.value : ''}
+                      onChange={(e) =>
+                        field.onChange(e.target.value.replace('$', ''))
+                      }
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type="submit">Submit</Button>
+          </form>
+        </Form>
+      </DialogContent>
+    </Dialog>
+  );
+}
